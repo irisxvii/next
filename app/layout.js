56169d67a4a7f993075bc3a40d2e22952e4a2785
irisxvii <iriskurien@gmail.com>
2024-10-03 01:43:34 +0530

    added font

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,12 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Fugaz_One } from '@next/font/google';
+
+
+const fugazOne = Fugaz_One({
+  weight: '400',  
+  subsets: ['latin'],  
+});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,20 +19,35 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+
 export const metadata = {
   title: "trackit",
   description: "track ur daily mood everyday of the year!",
 };
 
 export default function RootLayout({ children }) {
+
+  const header=(
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1 className={""+ fugazOne.className}>trackit</h1>
+    </header>
+  )
+
+  const footer=(
+    <footer>
+      footer
+    </footer>
+  )
+
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <header>header</header>
+         className={`w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col ${geistSans.variable} ${geistMono.variable} antialiased`}
+         style={{ minHeight: '100vh' }} >
+        {header}
         {children}
-        <footer>footer</footer>
+        <footer className="mt-auto"></footer>
+        {footer}
       </body>
     </html>
   );
