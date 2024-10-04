df658e0bb938c193a7dbc83569dc47ea3b065ae0
irisxvii <iriskurien@gmail.com>
2024-10-05 00:21:51 +0530

    login?

--- component/Login.js
+++ component/Login.js
@@ -1,7 +1,20 @@
+import { Fugaz_One } from '@next/font/google';
 import React from 'react'
+import Button from './Button';
+
+const fugazOne = Fugaz_One({
+  weight: '400',  
+  subsets: ['latin'],  
+});
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='flex flex-col flex-1 justify-center items-center gap-4'>
+      <h3 className={'subtext ' + fugazOne.className}>Log In / Register</h3>
+      <p>you're one step away!</p>
+      <input placeholder='Email'/>
+      <input placeholder='Password'/>
+      <Button text="Submit"/>
+    </div>
   )
 }
